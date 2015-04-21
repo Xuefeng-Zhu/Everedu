e6c7dd6350434438d84b2cbc9988f8e89b4eaad3
Xuefeng-Zhu <xzhu15@illinois.edu>
2015-04-21 18:52:08 -0500

    be able to save profile to firebase

--- web/src/js/controllers/UserCtrl.js
+++ web/src/js/controllers/UserCtrl.js
@@ -5,97 +5,99 @@
  * define controllers used to manage course list, and user profile
  */
 angular.module('everedu.UserCtrl', [])
-    // controller used to store course list info
-    .controller('UserCtrl', ['$scope', '$modal',
-        function($scope, $modal) {
-            $scope.courses = [{
-                courseID: 'CS242',
-                day: 'M',
-                time: '4:00pm-5:00pm'
-            }, {
-                courseID: 'CS425',
-                day: 'TR',
-                time: '9:30am-10:45am'
-            }];
+// controller used to store course list info
+.controller('UserCtrl', ['$scope', '$modal',
+    function($scope, $modal) {
+        $scope.courses = [{
+            courseID: 'CS242',
+            day: 'M',
+            time: '4:00pm-5:00pm'
+        }, {
+            courseID: 'CS425',
+            day: 'TR',
+            time: '9:30am-10:45am'
+        }];
 
-             /**
-             * @name openCourseModal
-             * @desc Open the modal used to create course, and push
-             * the new course to the course list
-             */
-            $scope.openCourseModal = function() {
-                var modal = $modal.open({
-                    templateUrl: 'CourseModal.html',
-                    controller: 'CreateCourseCtrl',
-                });
+        /**
+         * @name openCourseModal
+         * @desc Open the modal used to create course, and push
+         * the new course to the course list
+         */
+        $scope.openCourseModal = function() {
+            var modal = $modal.open({
+                templateUrl: 'CourseModal.html',
+                controller: 'CreateCourseCtrl',
+            });
 
-                modal.result.then(function(course) {
-                	$scope.courses.push(course);
-                });
-            }
+            modal.result.then(function(course) {
+                $scope.courses.push(course);
+            });
         }
-    ])
-    // controller used to create course
-    .controller('CreateCourseCtrl', ['$scope', '$modalInstance', '$filter',
-        function($scope, $modalInstance, $filter) {
-            $scope.$modalInstance = $modalInstance;
+    }
+])
+// controller used to create course
+.controller('CreateCourseCtrl', ['$scope', '$modalInstance', '$filter',
+    function($scope, $modalInstance, $filter) {
+        $scope.$modalInstance = $modalInstance;
 
-            $scope.course = {};
-            $scope.schedule = {
-            	day: {
-            		M: false,
-            		T: false,
-            		W: false,
-            		R: false,
-            		F: false
-            	}
-            };
-
-             /**
-             * @name create
-             * @desc Format new course data, and pass the data back
-             * to parent controller
-             */
-            $scope.create = function() {
-            	var day = "";
-                angular.forEach($scope.schedule.day, function(value, key){
-                    if (value){
-                        day += key;
-                    }
-                })
-
-            	$scope.course.day = day;
-
-                $scope.course.startTime = 
-                	$filter('date')($scope.schedule.startTime, 'h:mm a');
-                $scope.course.endTime = 
-                	$filter('date')($scope.schedule.endTime, 'h:mm a');
-
-                $modalInstance.close($scope.course);
+        $scope.course = {};
+        $scope.schedule = {
+            day: {
+                M: false,
+                T: false,
+                W: false,
+                R: false,
+                F: false
             }
-        }
-    ])
-    // controller used to manage user profile
-    .controller('ProfileCtrl', ['$scope', '$filter',
-        function($scope, $filter) {
-            $scope.editing = false;
-            $scope.user = {
-                name: 'Frank Zhu',
-                officeHour: 'Not Available'
-            }
+        };
 
-             /**
-             * @name edit
-             * @desc Edit the user profile
-             */
-            $scope.edit = function() {
-                if ($scope.editing) {
-                    $scope.user.name = $scope.copy.name;
-                    $scope.user.officeHour = $filter('date')($scope.copy.startTime, 'h:mm a');
+        /**
+         * @name create
+         * @desc Format new course data, and pass the data back
+         * to parent controller
+         */
+        $scope.create = function() {
+            var day = "";
+            angular.forEach($scope.schedule.day, function(value, key) {
+                if (value) {
+                    day += key;
                 }
+            })
 
-                $scope.copy = angular.copy($scope.user);
-                $scope.editing = !$scope.editing;
-            }
+            $scope.course.day = day;
+
+            $scope.course.startTime =
+                $filter('date')($scope.schedule.startTime, 'h:mm a');
+            $scope.course.endTime =
+                $filter('date')($scope.schedule.endTime, 'h:mm a');
+
+            $modalInstance.close($scope.course);
         }
-    ])
+    }
+])
+// controller used to manage user profile
+.controller('ProfileCtrl', ['$scope', '$filter', 'Profile',
+    function($scope, $filter, Profile) {
+        $scope.editing = false;
+        $scope.profile = Profile($scope.uid)
+
+        /**
+         * @name edit
+         * @desc Edit the user profile
+         */
+        $scope.edit = function() {
+            if ($scope.editing) {
+                $scope.profile.name = $scope.copy.name;
+                if ($scope.copy.officeHour) {
+                    $scope.profile.officeHour =
+                        $filter('date')($scope.copy.officeHour, 'h:mm a');
+                }
+                $scope.profile.$save();
+            } else {
+                $scope.copy = angular.copy($scope.profile);
+            }
+            
+            $scope.editing = !$scope.editing;
+        }
+    }
+])
